84f8fe3cfe60a7a670baa5c33fd28d84c9841077
Rune Espenakk <143501123+espenakk@users.noreply.github.com>
2025-05-05 09:42:26 +0200

    Added cube pointer package

---
+++ src/ur_cube_pointer/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'ur_cube_pointer'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rune',
+    maintainer_email='143501123+espenakk@users.noreply.github.com',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'camera_node = ur_cube_pointer.camera_node:main'
+        ],
+    },
+)
---
+++ src/ur_cube_pointer/ur_cube_pointer/camera_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ur_cube_pointer.')
+
+
+if __name__ == '__main__':
+    main()
